841bf4f33d606920383ce6695f8d744cd55fa356
Jiří Šuster <suster.ji@gmail.com>
2024-04-05 15:14:10 +0200

    Merge adjustments

--- merge.py
+++ merge.py
@@ -7,5 +7,14 @@ data2 = pd.read_csv('student-por.csv')
 # Merge the data
 data = pd.concat([data1, data2])
 
+# Remove "Nursery" column
+data = data.drop(columns=['nursery'])
+
+# Remove "School" column
+data = data.drop(columns=['school'])
+
+# Remove "Reason" column
+data = data.drop(columns=['reason'])
+
 # Save the data
 data.to_csv('student.csv', index=False)
